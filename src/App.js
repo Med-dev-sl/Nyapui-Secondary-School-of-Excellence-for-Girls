784c52244676edcae673868345a1c6791c985782
Med-dev-sl <mohamedsallu24@gmail.com>
2026-04-08 02:50:38 +0000

    resized the logo for better visibility

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Logo from './components/Logo';
 function App() {
   return (
     <div className="App">
-      <Logo className="App-logo" alt="App Logo" width="100" height="100" />
+      <Logo className="App-logo" alt="App Logo" width="200" />
     </div>
   );
 }
